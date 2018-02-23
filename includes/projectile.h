11b5eadcdf641afcbdb3d29a9e7011ed7c063db8
Xiaoming <mail@xiaoming.se>
2018-02-23 23:14:20 +0100

    Add class for Projectile

---
+++ includes/projectile.h
@@ -0,0 +1,26 @@
+#ifndef PROJECTILE_H
+#define PROJECTILE_H
+enum Direction
+{
+    UP;
+    DOWN;
+    LEFT;
+    RIGHT;
+};
+
+class ProjectileBehaviourComponent : public Component
+{
+public:
+    void Update(float dt,Direction dir);
+};
+
+
+// rockets are shot by the player towards the aliens
+class Projectile : public GameObject
+{
+public:
+    virtual void Init(double xPo,double yPo);
+    virtual void Receive(Message m);
+    virtual Direction getDirection();
+};
+#endif // PROJECTILE_H
---
+++ src/projectile.cpp
@@ -0,0 +1,44 @@
+#include "projectile.h"
+
+void ProjectileBehaviourComponent::Update(float dt, Direction dir){
+    switch (dir)
+    {
+    case Direction.UP:
+        go->verticalPosition -= ROCKET_SPEED * dt;
+        break;
+    case Direction.DOWN:
+        go->verticalPosition += ROCKET_SPEED * dt;
+        break;
+    case Direction.LEFT:
+        go->horizontalPosition -= ROCKET_SPEED * dt;
+        break;
+    case Direction.RIGHT:
+        go->horizontalPosition += ROCKET_SPEED * dt;
+        break;
+    default: break;
+    }
+    //Check out of bounds
+    if(go->horizontalPosition > 480 ||
+       go->horizontalPosition < 0 ||
+       go->verticalPosition > 640 ||
+       go->verticalPosition < 0)
+        go->enabled = false;
+}
+
+void Projectile::Init(double xPo,yPo)
+{
+    SDL_Log("Projectile::Init");
+    GameObject::Init();
+    horizontalPosition = xPo;
+    verticalPosition = yPo;
+}
+
+void Projectile::Receive(Message m)
+{
+    if(enabled && m == HIT)
+    {
+        enabled = false;
+        SDL_Log("Projectile::HIT");
+    }
+}
+
