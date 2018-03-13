136de04b597a995884b7a08f110b3304fb8f6886
Xiaoming <mail@xiaoming.se>
2018-03-13 20:03:49 +0100

    Remove unnecessary code

--- include/constants.h
+++ include/constants.h
@@ -10,7 +10,6 @@ const unsigned int	NUM_LIVES = 2;
 
 const unsigned int	MAX_NUM_ROCKETS = 32;
 const unsigned int	MAX_NUM_BOMBS = 32;
-const unsigned int	POINTS_PER_ALIEN = 100;
 const float			FIRE_TIME_INTERVAL = .5f;
 const float			BOMB_TIME_INTERVAL = 1.25f;
 const float			PLAYER_SPEED = 160.0f;
