5eb50ea1dfa8ca5e062d48b7f644b66c7a87cfcb
Xiaoming <mail@xiaoming.se>
2018-03-14 23:32:31 +0100

    Change fire interval

--- include/constants.h
+++ include/constants.h
@@ -10,7 +10,7 @@ const unsigned int	NUM_LIVES = 2;
 
 const unsigned int	MAX_NUM_ROCKETS = 32;
 const unsigned int	MAX_NUM_BOMBS = 32;
-const float			FIRE_TIME_INTERVAL = .5f;
+const float			FIRE_TIME_INTERVAL = .2f;
 const float			BOMB_TIME_INTERVAL = 1.25f;
 const float			PLAYER_SPEED = 160.0f;
 const float			ROCKET_SPEED = 160.0f;
